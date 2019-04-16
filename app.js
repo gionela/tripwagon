23edcf93b5121b5f7511e389cd2224bc747352ae
Gionela Mita <giwnmita@hotmail.com>
2019-04-16 14:48:40 +0300

    Datalist dynamically created

--- app.js
+++ app.js
@@ -6,28 +6,40 @@ function init($) {
         url: "data.json",
         success: jsonHandler
     }
+    //remove duplicates from array
+    function removeDups(names) {
+        let unique = {};
+        names.forEach(function (i) {
+            if (!unique[i]) {
+                unique[i] = true;
+            }
+        });
+        return Object.keys(unique);
+    }
+
+    function createOptions(listOfCities){
+        //1) get datalist
+        let dataList = document.querySelector("#location");
+        //2) loop over uniqueCities array
+        listOfCities.map(addOption);
+
+        function addOption(city){
+            dataList.innerHTML += `<option value="${city}"></option>`
+        }
+    }
 
     function jsonHandler(data) {
-        console.log(data);
         let entries = data[1].entries;
         let cities = entries.map(getCity);
+        let uniqueCities = removeDups(cities);
 
         function getCity(hotel) {
             return hotel.city;
         }
-//remove duplicates from array
-        function removeDups(names) {
-            let unique = {};
-            names.forEach(function (i) {
-                if (!unique[i]) {
-                    unique[i] = true;
-                }
-            });
-            return Object.keys(unique);
-        }
-        let uniqueCities = removeDups(cities);
         console.log(uniqueCities);
 
+        createOptions(uniqueCities);
+   
     }
     $.ajax(options);
 }
